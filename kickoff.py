cede05eddb7437bf74364da9c4d823c2a45774a2
Philip Bove <pgbson@gmail.com>
2016-08-13 02:06:03 -0400

    things

--- kickoff.py
+++ kickoff.py
@@ -1,3 +1,5 @@
+
+#TODO: ssh func tomorrow, write unit tests and add exception handling 8/10/2016
 import winrm #to be used for powershell/windows commands
 import paramiko
 import configparser
@@ -11,6 +13,7 @@ pasrer.add_argument('-b', '--ball', usage= 'ball you want to use', required=True
 parser.add_argument('-m','--machine', usage= 'machine adress' )
 parser.add_argument('-g', '--group', useage= 'run ball against group from inventory file')
 parser.add_argument('-h', '--help', usage= 'help', action= 'store_true')
+parser.add_argument('-k', '--key', usage='select ssh key to use')
 #QUICK AND DIRTY, USING GLOBALS (NOT BEST PRACTICE, PLS NO KILL)
 #
 config = configparser.ConfigParser()
@@ -69,21 +72,23 @@ def list_balls():
 		print(conifg.get(section,'description'))
 		print('--------------------------------')	
 
-def runner(trigger):
+def runner(group): #might combine with tag_check
 	steps = sort_balls
-	if trigger == 'group':
-		group = inv.options(args.group)
-		if tag_check() == 'windows'
-			for machine in group:
-				winrm_connect(machine, steps)
+ 	if tag_check() == 'windows'
+ 		count = 0
+		for machine in group:
+			count = count + 1
+			winrm_connect(machine, steps, count)
+	if tag_check() == 'linux'
+		for machine in group:
+			ssh_connect(machine)
 
-def winrm_connect(machine, steps):
-	count = 0
+
+def winrm_connect(machine, steps, count):
 	user = input('Enter username [>] ')
 	passwrd = getpass.getpass('Enter password [>] ')
 	session = winrm.Session(machine, auth=(user, passwrd))
 	for step in steps:
-		count = count +1 
 		execute = session.run_cmd(step)
 		if count == 1:
 			print(execute.std_out)
@@ -95,10 +100,32 @@ def winrm_connect(machine, steps):
 	print('[>] Success!')
 	print('[>] Log saved')
 
+def ssh_connect(machine, steps, count):
+	client = paramiko.client.Client()
+	client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	if args.key is None:
+		print('[>] No key selected for ssh')
+		sys.exit()
+	try:
+		client.connect(machine, pkey=args.key)
+	except paramiko.ssh_exception.AuthenticationException:
+		print('[>] Auth error, check target machine name/ip address')
+		sys.exit()
+	for step in steps:
+		stdin,stdout, stderr = client.exec_command(step)
+		if count == 1:
+			print(stdout)
+		if stderr is not None: #this probably will get changed to len or something
+			print('----------------------------')
+			print(std_err)
+			sys.exit()
+	print('[>] Success!')
+	print('[>] Log Saved')
 
-def ssh_connect():
 
-def sort_balls():
+
+
+def sort_balls(): #replace with list comprehension
 	steps = [];
 	options = config.options(args.ball)
 	options.remove('description')
@@ -110,7 +137,7 @@ def sort_balls():
 def tag_check():
 	if config.get(args.ball, 'tag') == 'windows':
 		return 'windows'
-	elif config.get(args.ball, 'tag') == 'linux'
+	elif config.get(args.ball, 'tag') == 'linux':
 		return 'linux'
 
 def generate_log():
